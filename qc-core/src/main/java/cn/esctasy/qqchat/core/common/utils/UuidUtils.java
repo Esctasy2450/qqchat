21c70f9b663dcf6eed020de838a3081ae241f0a5
zhaoran <3071521352@qq.com>
2024-02-25 17:07:06 +0800

    移除冗余方法

--- qc-core/src/main/java/cn/esctasy/qqchat/core/common/utils/UuidUtils.java
+++
@@ -1,9 +0,0 @@
-package cn.esctasy.qqchat.core.common.utils;
-
-import java.util.UUID;
-
-public class UuidUtils {
-    public static String id() {
-        return UUID.randomUUID().toString().replace("-", "");
-    }
-}
