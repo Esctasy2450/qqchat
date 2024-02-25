76568cd0af15d47f96e9bf6e549b09c80e37fcbf
zhaoran <3071521352@qq.com>
2024-02-25 17:36:42 +0800

    移除所有core中关于bard的代码

--- qc-core/src/main/java/cn/esctasy/qqchat/core/event/PrivateMsg.java
+++ qc-core/src/main/java/cn/esctasy/qqchat/core/event/PrivateMsg.java
@@ -1,8 +1,6 @@
 package cn.esctasy.qqchat.core.event;
 
 import cn.esctasy.qqchat.api.reply.bean.Reply;
-import com.pkslow.ai.GoogleBardClient;
-import com.pkslow.ai.domain.Answer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,17 +11,9 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/private")
 public class PrivateMsg {
-    private final GoogleBardClient client;
 
     @GetMapping("/send")
     public void send(@RequestBody Map<String, Object> param, @RequestParam String action) {
         Reply.build(action, param).send();
-    }
-
-    @GetMapping("/ask")
-    public String ask(@RequestParam("q") String question) {
-        Answer answer = client.ask(question);
-        System.out.println(answer);
-        return answer.getChosenAnswer();
     }
 }
