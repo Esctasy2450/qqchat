25b823c43802eee11fc51ed5aae31e730ecced20
zhaoran <3071521352@qq.com>
2024-03-04 17:48:27 +0800

    增加数据持久化使用的接口

---
+++ qc-api/src/main/java/cn/esctasy/qqchat/api/db/QCDBBaseMapper.java
@@ -0,0 +1,7 @@
+package cn.esctasy.qqchat.api.db;
+
+/**
+ * 基础增删改查接口
+ */
+public interface QCDBBaseMapper {
+}
---
+++ qc-api/src/main/java/cn/esctasy/qqchat/api/db/QQChatDS.java
@@ -0,0 +1,8 @@
+package cn.esctasy.qqchat.api.db;
+
+/**
+ * 数据自动持久化切面所使用的注解
+ * Data Storage 简写 DS
+ */
+public @interface QQChatDS {
+}
