9c23148fc6a9ac2ec99222599f41401f95179af1
zhaoran <3071521352@qq.com>
2024-03-05 16:59:45 +0800

    修改自动装配路径

--- qc-bot/src/main/java/cn/esctasy/qqchat/bot/config/BotAutoConfiguration.java
+++ qc-bot/src/main/java/cn/esctasy/qqchat/bot/config/BotAutoConfiguration.java
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Configuration;
  * 自动装配类，引入bot模块的同时自动装配
  * */
 @Configuration
-@ComponentScan(value = "cn.esctasy.qqchat.bot.event")
+@ComponentScan(value = "cn.esctasy.qqchat.bot")
 public class BotAutoConfiguration {
 }
