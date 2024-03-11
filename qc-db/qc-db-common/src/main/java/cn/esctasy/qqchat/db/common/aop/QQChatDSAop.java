a18b8d2a312de680ffa354fd948c086feea1094e
zhaoran <3071521352@qq.com>
2024-03-11 18:22:14 +0800

    切面方法内获取指定接口的手段，为持久化做铺垫

--- qc-db/qc-db-common/src/main/java/cn/esctasy/qqchat/db/common/aop/QQChatDSAop.java
+++ qc-db/qc-db-common/src/main/java/cn/esctasy/qqchat/db/common/aop/QQChatDSAop.java
@@ -2,29 +2,87 @@ package cn.esctasy.qqchat.db.common.aop;
 
 import cn.esctasy.qqchat.api.db.QQChatDSHandleForAop;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.*;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class QQChatDSAop {
     private final QQChatDSHandleForAop qqChatDSHandleForAop;
-    
+
     @Pointcut("execution(* cn.esctasy.qqchat.api.event.handle..*.eventHandle(..))")
     public void point() {
     }
-    
+
     @Before("point()")
-    public void before() {
+    public void before(JoinPoint joinpoint) {
+        System.out.println("----------Before开始-----------");
+
+        System.out.println("方法名：" + joinpoint.getSignature().getName());
+        System.out.println("参数值集合：" + Arrays.asList(joinpoint.getArgs()));
+        System.out.println("参数值类型：" + joinpoint.getArgs()[0].getClass().getTypeName());
+        // 获取目标注解对象，Cacheab  leTest是自定义的一个注解
+        String cacheable = ((MethodSignature) joinpoint.getSignature()).getName();
+        String classType = joinpoint.getTarget().getClass().getName();
+        System.out.println("目标注解对象：" + cacheable);
+        System.out.println("获取目标方法所在类：" + classType);
+        System.out.println(Arrays.toString(joinpoint.getTarget().getClass().getInterfaces()));
+
+        Class<?>[] interfaces = findInterfacesWithMethod(joinpoint.getTarget().getClass(), joinpoint.getSignature().getName());
+        for (Class<?> intf : interfaces) {
+            System.out.println(intf.getName());
+        }
+        System.out.println("----------Before结束-----------");
+
         qqChatDSHandleForAop.before();
     }
-    
+
     @After("point()")
     public void after() {
         qqChatDSHandleForAop.after();
+    }
+
+
+    public static Class<?>[] findInterfacesWithMethod(Class<?> targetClass, String methodName) {
+        Method[] methods = targetClass.getMethods();
+        for (Method method : methods) {
+            if (method.getName().equals(methodName)) {
+                Class<?>[] interfaces = targetClass.getInterfaces();
+                return filterInterfacesWithMethod(interfaces, methodName);
+            }
+        }
+        return new Class<?>[0];
+    }
+
+    public static Class<?>[] filterInterfacesWithMethod(Class<?>[] interfaces, String methodName) {
+        int count = 0;
+        for (Class<?> intf : interfaces) {
+            try {
+                intf.getMethod(methodName);
+                count++;
+            } catch (NoSuchMethodException ignore) {
+            }
+        }
+        Class<?>[] result = new Class<?>[count];
+        count = 0;
+        for (Class<?> intf : interfaces) {
+            try {
+                intf.getMethod(methodName);
+                result[count++] = intf;
+            } catch (NoSuchMethodException ignore) {
+                // Ignore interfaces that don't contain the method
+            }
+        }
+        return result;
     }
 }
