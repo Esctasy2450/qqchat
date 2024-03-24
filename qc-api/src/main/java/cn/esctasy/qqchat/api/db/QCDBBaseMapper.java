9f90afd9a16f13c5021227ebb479ff780ae71538
zhaoran <3071521352@qq.com>
2024-03-25 00:00:06 +0800

    增加基础代码，实现待增加，

--- qc-api/src/main/java/cn/esctasy/qqchat/api/db/QCDBBaseMapper.java
+++ qc-api/src/main/java/cn/esctasy/qqchat/api/db/QCDBBaseMapper.java
@@ -2,6 +2,14 @@ package cn.esctasy.qqchat.api.db;
 
 /**
  * 基础增删改查接口
+ * todo
  */
 public interface QCDBBaseMapper {
+    <T> void add(T obj);
+
+    void update();
+
+    void select();
+
+    void delete();
 }
---
+++ qc-db/qc-db-quickio/src/main/java/cn/esctasy/qqchat/db/quickio/QCDBBaseMapperImpl.java
@@ -0,0 +1,33 @@
+package cn.esctasy.qqchat.db.quickio;
+
+import cn.esctasy.qqchat.api.db.QCDBBaseMapper;
+import com.github.artbits.quickio.core.IOEntity;
+import org.springframework.stereotype.Service;
+
+/**
+ * 基础增删改查实现类
+ */
+@Service
+public class QCDBBaseMapperImpl implements QCDBBaseMapper {
+
+
+    @Override
+    public <T> void add(T obj) {
+        obj.getClass();
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void select() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+}
