33502984329b3b9aca0e86685011d92f856a2496
zhaoran <3071521352@qq.com>
2024-03-24 23:59:11 +0800

    增加quickio基础增删改查

---
+++ qc-db/qc-db-quickio/src/main/java/cn/esctasy/qqchat/db/quickio/BaseMapper.java
@@ -0,0 +1,73 @@
+package cn.esctasy.qqchat.db.quickio;
+
+import cn.esctasy.qqchat.db.quickio.config.QuickIoConfig;
+import cn.hutool.extra.spring.SpringUtil;
+import com.github.artbits.quickio.api.Collection;
+import com.github.artbits.quickio.api.DB;
+import com.github.artbits.quickio.core.Config;
+import com.github.artbits.quickio.core.IOEntity;
+import com.github.artbits.quickio.core.QuickIO;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseMapper {
+    private static final DB db = QuickIO.usingDB(SpringUtil.getBean(QuickIoConfig.class).getConfig());
+
+    /**
+     * 保存
+     */
+    public <T extends IOEntity> void save(T t, Class<T> clazz) {
+        Collection<T> collection = db.collection(clazz);
+        collection.save(t);
+    }
+
+    /**
+     * 批量保存
+     */
+    public <T extends IOEntity> void save(List<T> t, Class<T> clazz) {
+        Collection<T> collection = db.collection(clazz);
+        collection.save(t);
+    }
+
+    /**
+     * 根据主键查询
+     */
+    public <T extends IOEntity> T selectById(long id, Class<T> clazz) {
+        if (0L == id) {
+            return null;
+        }
+
+        Collection<T> collection = db.collection(clazz);
+        List<T> list = collection.find(id);
+        if (CollectionUtils.isEmpty(list)) {
+            return null;
+        }
+
+        return list.get(0);
+    }
+
+    /**
+     * 根据主键批量查询
+     */
+    public <T extends IOEntity> List<T> selectByIds(Class<T> clazz, long... id) {
+        Collection<T> collection = db.collection(clazz);
+        List<T> list = collection.find(id);
+        if (CollectionUtils.isEmpty(list)) {
+            return new ArrayList<>();
+        }
+
+        return list;
+    }
+
+    /**
+     * 根据主键删除
+     */
+    public <T extends IOEntity> int delById(Class<T> clazz, long... ids) {
+        Collection<T> collection = db.collection(clazz);
+        collection.delete(ids);
+        
+        return ids.length;
+    }
+}
---
+++ qc-db/qc-db-quickio/src/main/java/cn/esctasy/qqchat/db/quickio/config/QuickIoConfig.java
@@ -0,0 +1,19 @@
+package cn.esctasy.qqchat.db.quickio.config;
+
+import com.github.artbits.quickio.core.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "esctasy.qqchat.quick-io")
+public class QuickIoConfig {
+    private String name = "qqchat";
+    private String path = "data/qio";
+    private Long cacheSize = 16L * 1024 * 1024;
+    
+    public Config getConfig() {
+        return Config.of(c -> {
+            c.name("qqchat");
+            c.path("data/qio");                 // Custom base path.
+            c.cache(16L * 1024 * 1024);         // Set cache size.
+        });
+    }
+}
