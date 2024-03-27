e999cb3b6786d0cfedd1b9aecd53c1ea756d57b7
朝朝 <3071521352@qq.com>
2024-03-27 22:30:49 +0800

    留一个后续

--- qc-api/src/main/java/cn/esctasy/qqchat/api/db/QCDBBaseMapper.java
+++ qc-api/src/main/java/cn/esctasy/qqchat/api/db/QCDBBaseMapper.java
@@ -3,6 +3,7 @@ package cn.esctasy.qqchat.api.db;
 /**
  * 基础增删改查接口
  * todo
+ * 此处有问题，因为quickIo需要继承IOEntity类才可以使用，导致mysql之类的关系型数据库在切换时暂时没有办法无感切换
  */
 public interface QCDBBaseMapper {
     <T> void add(T obj);
